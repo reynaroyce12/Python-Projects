39dc9fe504c6575a1ad6a3109e793c50f4c282ae
Reyna Royce <80285415+reynaroyce12@users.noreply.github.com>
2021-11-22 23:35:47 +0530

    add exception handling

--- pandas/NATOAlphabets/main.py
+++ pandas/NATOAlphabets/main.py
@@ -4,6 +4,16 @@ data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user_input = input("Enter the word: ").upper()
-output = [phonetic_dict[letter] for letter in user_input]
-print(f"Your phonetic alphabets are {output}")
+def generate():
+    user_input = input("Enter the word: ").upper()
+    try:
+        output = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("No numbers! Enter alphabets please")
+        generate()
+    else:
+        print(f"Your phonetic alphabets are {output}")
+
+
+generate()
+
